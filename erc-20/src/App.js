aaecad58d59b30f3b35993c6375fa9eff3fcdf6e
Gaurav Kumar <blockhub0123@gmail.com>
2026-07-08 04:56:54 +0530

    Updated App.js, Sell and transfer functionality

--- erc-20/src/App.js
+++ erc-20/src/App.js
@@ -1,21 +1,57 @@
 import "./App.css";
+import { CONTRACT_ADDRESS,CONTRACT_ABI } from "./Contract_Address.js";
 import BuyTokens from "./Component/BuyToken";
 import SellToken from "./Component/SellToken.js";
-import Wallet from "./Component/Wallet_info.js";
 import Transfer from "./Component/Transfer.js";
 import { useState } from "react";
+import { useEffect } from "react";
+const {ethers} = require("ethers");
 function App() {
-    const [Contract,SetContract] = useState("");
-  const get=async (contract) => {
-      // console.log("Address : ",contract);
-      SetContract(contract)
-  }
+  const [Contract, setContract] = useState(null);
+  const [Contract_Token, SetToken] = useState("");
+  const [Token_Symbol, SetSymbol] = useState("");
+  const [Json,SetJson]=useState("");
+  useEffect(() => {
+    const Token = async () => {
+      const jsnoRpc = new ethers.JsonRpcProvider(
+        "https://sepolia.infura.io/v3/6b02f694eadd41f283326ca4912ccce9",
+      );
+      const c1 = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, jsnoRpc);
+      const Token_Name = await c1.tokenName();
+      SetJson(c1);
+      SetToken(Token_Name);
+      const Symbol = await c1.symbol();
+      SetSymbol(Symbol);
+    };
+    Token();
+  }, []);
+  const connect2 = async () => {
+    try {
+      if (!window.ethereum) {
+        window.alert("Please install Meta Mask");
+        return;
+      }
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        CONTRACT_ABI,
+        signer,
+      );
+      setContract(contract);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="App">
-      <Wallet sendContract={get}/>
-      <BuyTokens Contract={Contract}/>
-      <SellToken Contract={Contract}/>
-      <Transfer Contract={Contract}/>
+      <h1>{Contract_Token}</h1>
+      <h2>{Token_Symbol}</h2>
+      <button onClick={connect2}>Connect</button>
+      <BuyTokens Contract={Contract} />
+      <SellToken Contract={Contract} />
+      <Transfer Contract={Contract} Json={Json}/>
     </div>
   );
 }
--- erc-20/src/Component/Transfer.js
+++ erc-20/src/Component/Transfer.js
@@ -1,31 +1,30 @@
 import { useState } from "react";
 const { ethers } = require("ethers");
-function Transfer({ Contract }) {
+function Transfer({ Contract, Json }) {
   const [Input, setInput] = useState("");
   const [Check, setBalance] = useState("");
+  const [Input_Token, SetToken] = useState("");
   const Call_Transfer = async () => {
     try {
-      const Send = await Contract.transfer(Input, 1000);
-      console.log("Token Name :", Send);
-
-      Contract.on("Transfer", (sender, reciver, amount) => {
-        console.log("Sender :", sender);
-        console.log("Reciver :", reciver);
-        console.log("Amount :", Number(amount));
-      });
+      console.log("TransferContract :", Contract);
+      const deci = await Json.decimal();
+      const c2 = await ethers.parseUnits(Input_Token, deci);
+      const Send = await Contract.transfer(Input, c2);
       await Send.wait();
+      console.log("Token Name :", Send);
     } catch (err) {
       console.error(err);
     }
   };
   const BalanceOf = async () => {
-    const Token = await Contract.balanceof(Check);
+    console.log("Contract :", Json);
+    const Token = await Json.balanceof(Check);
 
     console.log("Balance of Buyer :", ethers.formatEther(Token));
     setBalance("");
   };
   return (
-    <div className="transfer_css">
+    <div className="transfer_css"> 
       <button onClick={Call_Transfer}>Transfer</button>
       <input
         text="text"
@@ -35,8 +34,16 @@ function Transfer({ Contract }) {
           setInput(e.target.value);
         }}
       />
-      <br/>
-      <br/>
+      <input
+        text="text"
+        placeholder="Enter Token"
+        value={Input_Token}
+        onChange={(e) => {
+          SetToken(e.target.value);
+        }}
+      />
+      <br />
+      <br />
       <button onClick={BalanceOf}>CheckBalance</button>
       <input
         text="text"
